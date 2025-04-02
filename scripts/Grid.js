501167819e744ae9124f27314f9be09600b8dd6b
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-04-02 14:04:17 -0300

    implementando Grid e Tile classes, criando os cell elements e fazendo um update na logica do game board

--- scripts/Grid.js
+++ scripts/Grid.js
@@ -1,13 +1,67 @@
-const GRID_SIZE = 4
-const CELL_SIZE = 20
-const CELL_GAP = 2
-const CELL_RADIUS = 1
+const GRID_SIZE = 4;
+const CELL_SIZE = 20;
+const CELL_GAP = 2;
+const CELL_RADIUS = 1;
 
 export default class Grid {
-    constructor(elementoGrid){
-    elementoGrid.style.setProperty("--grid-size", GRID_SIZE)
-    elementoGrid.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
-    elementoGrid.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
-    elementoGrid.style.setProperty("--cell-radius", `${CELL_RADIUS}vmin`)
-    }
+  #cells;
+
+  constructor(elementoGrid) {
+    elementoGrid.style.setProperty("--grid-size", GRID_SIZE);
+    elementoGrid.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
+    elementoGrid.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
+    elementoGrid.style.setProperty("--cell-radius", `${CELL_RADIUS}vmin`);
+    this.#cells = createCellElements(elementoGrid).map(
+      (elementoCell, index) => {
+        return new Cell(
+          elementoCell,
+          index % GRID_SIZE,
+          Math.floor(index / GRID_SIZE)
+        );
+      }
+    );
+  }
+
+  get #emptyCells() {
+    return this.#cells.filter((cell) => cell.tile == null);
+  }
+
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+    return this.#emptyCells[randomIndex];
+  }
+}
+
+class Cell {
+  #elementoCell;
+  #x;
+  #y;
+  #tile;
+
+  constructor(elementoCell, x, y) {
+    this.#elementoCell = elementoCell;
+    this.#x = x;
+    this.#y = y;
+  }
+  get tile() {
+    return this.#tile;
+  }
+
+  set tile(value) {
+    this.#tile = value;
+    if (value == null) return;
+    this.#tile.x= this.#x;
+    this.#tile.y= this.#y;
+  }
+}
+
+function createCellElements(elementoGrid) {
+  const cells = [];
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cells.push(cell);
+    elementoGrid.append(cell);
+  }
+  return cells;
 }
--- scripts/Tile.js
+++ scripts/Tile.js
@@ -1,1 +1,32 @@
-export default class Tile {}
+export default class Tile {
+ #elementoTile
+ #x
+ #y
+ #value
+
+constructor(containerTile, value= Math.random() > 0.5 ? 2 : 4) {
+    this.#elementoTile = document.createElement("div")
+    this.#elementoTile.classList.add("tile")
+    containerTile.append(this.#elementoTile)
+    this.value = value
+    }
+
+ set value(v) {
+    this.#value = v
+    this.#elementoTile.textContent = v
+    const power = Math.log2(v)
+    const backgroundLightness = 100 - power * 9
+    this.#elementoTile.style.setProperty("--background-lightness", `${backgroundLightness}%`)
+    this.#elementoTile.style.setProperty("--text-lightness", `${backgroundLightness < 50 ? 90 : 10}%`)
+    }
+
+set x(value) {
+    this.#x = value
+    this.#elementoTile.style.setProperty("--x", value)
+    }
+
+set y(value) {
+    this.#y = value
+    this.#elementoTile.style.setProperty("--y", value)
+    }
+}
